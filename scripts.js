31a7f5fb8406c24d220e5192e7c9b501d8d51879
theus-2005 <161030324+theus-2005@users.noreply.github.com>
2024-04-09 17:37:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showPage2() {
+    document.getElementById("page1").classList.add("hidden");
+    document.getElementById("page2").classList.remove("hidden");
+  }
+  
+  function showPage1() {
+    document.getElementById("page2").classList.add("hidden");
+    document.getElementById("page1").classList.remove("hidden");
+  }
+  
